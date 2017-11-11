f59609745e465fe0f31a2b60422baa72cba323b7
Hannah Lily Postman <postman.hl@gmail.com>
2017-11-11 12:37:28 -0800

    Milestone 2 done.  Note had to update to correct version of AlomofireImage

--- TumblerMockUp/PhotosViewController.swift
+++ TumblerMockUp/PhotosViewController.swift
@@ -27,9 +27,10 @@ class PhotosViewController: UIViewController {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
                 print(dataDictionary)
                 
-                // TODO: Get the posts and store in posts property
-                
-                // TODO: Reload the table view
+                // Get the dictionary from the response key
+                let responseDictionary = dataDictionary["response"] as! [String: Any]
+                // Store the returned array of dictionaries in our posts property
+                self.posts = responseDictionary["posts"] as! [[String: Any]]
             }
         }
         task.resume()
