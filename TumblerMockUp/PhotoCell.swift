93d849f5ff377faa5dacfafcf1a00d4c871e820d
Hannah Lily Postman <postman.hl@gmail.com>
2017-11-11 18:51:15 -0800

    Milestone 2 done. Scaffold tableview and custom cell

---
+++ TumblerMockUp/PhotoCell.swift
@@ -0,0 +1,32 @@
+//
+//  PhotoCell.swift
+//  TumblerMockUp
+//
+//  Created by Aristotle on 2017-11-11.
+//  Copyright © 2017 HLPostman. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell") as! PhotoCell
+        
+        // Configure photoCell using the outlets that you've defined.
+        
+        return cell
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- TumblerMockUp/PhotosViewController.swift
+++ TumblerMockUp/PhotosViewController.swift
@@ -8,14 +8,19 @@
 
 import UIKit
 
-class PhotosViewController: UIViewController {
+class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     var posts: [[String: Any]] = []
+    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // TableView setup
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         // Network request snippet
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -36,7 +41,17 @@ class PhotosViewController: UIViewController {
         task.resume()
 
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "This is row \(indexPath.row)"
+        
+        return cell
+    }
+	
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
